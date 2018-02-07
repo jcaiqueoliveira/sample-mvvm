75ba6f8883d68ced80a17bfe1de61c240a496031
Caique Oliveira <j.caique_fla@hotmail.com>
2018-02-07 15:30:57 -0200

    add some extensions update icon fab

--- domain/src/main/java/sample/kanda/domain/useCase.kt
+++ domain/src/main/java/sample/kanda/domain/useCase.kt
@@ -3,10 +3,16 @@ package sample.kanda.domain
 /**
  * Created by jcosilva on 2/5/2018.
  */
-class ContactManagerUseCase(val dataSource: LocalRepository) {
+class ContactManagerUseCase(private val dataSource: LocalRepository) {
 
     fun saveContact(person: Contact) {
         dataSource.addContact(person)
     }
+
+}
+
+
+class HomeUseCase(private val datasource: FieldsRepository) {
+
 
 }
