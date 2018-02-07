9bae8b20a9ab713affaa06fec1bd4d3ab7a4cea9
Caique Oliveira <j.caique_fla@hotmail.com>
2018-02-07 15:31:19 -0200

    add home injector

--- domain/src/main/java/sample/kanda/domain/UseCase.kt
+++ domain/src/main/java/sample/kanda/domain/UseCase.kt
@@ -3,16 +3,10 @@ package sample.kanda.domain
 /**
  * Created by jcosilva on 2/5/2018.
  */
-class ContactManagerUseCase(private val dataSource: LocalRepository) {
+class ContactManagerUseCase(val dataSource: LocalRepository) {
 
     fun saveContact(person: Contact) {
         dataSource.addContact(person)
     }
-
-}
-
-
-class HomeUseCase(private val datasource: FieldsRepository) {
-
 
 }
