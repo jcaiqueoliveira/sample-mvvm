49958b190de50e9f4e4f5126efb3b8d746583565
jcaiqueoliveira <j.caique_fla@hotmail.com>
2018-02-16 22:40:41 -0200

    remove singleton room

--- data/src/main/java/sample/kanda/data/local/AppDataBase.kt
+++ data/src/main/java/sample/kanda/data/local/AppDataBase.kt
@@ -4,12 +4,23 @@ import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
+import android.content.ContentValues
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import sample.kanda.data.FieldsDatabase.LABEL_CNPJ
+import sample.kanda.data.FieldsDatabase.LABEL_COMPANY_NAME
+import sample.kanda.data.FieldsDatabase.LABEL_EMAIL
+import sample.kanda.data.FieldsDatabase.LABEL_ID
+import sample.kanda.data.FieldsDatabase.LABEL_NAME
+import sample.kanda.data.FieldsDatabase.LABEL_NEGATIVE_OPTION
+import sample.kanda.data.FieldsDatabase.LABEL_PHONE
+import sample.kanda.data.FieldsDatabase.LABEL_POSITIVE_OPTION
+import sample.kanda.data.FieldsDatabase.LABEL_TITLE_MEI
+import sample.kanda.data.FieldsDatabase.TABLE_LABEL
 import sample.kanda.data.local.contact.ContactDao
 import sample.kanda.data.local.contact.ContactEntity
 import sample.kanda.data.local.contact.LabelDao
 import sample.kanda.data.local.contact.LabelEntity
-import java.util.concurrent.Executors
 
 /**
  * Created by caique on 2/16/18.
@@ -25,44 +36,36 @@ abstract class AppDataBase : RoomDatabase() {
 
 
     companion object {
-        //https://gist.github.com/florina-muntenescu/697e543652b03d3d2a06703f5d6b44b5
-        @Volatile
-        private var INSTANCE: AppDataBase? = null
-
-        fun getInstance(context: Context): AppDataBase {
-            return INSTANCE ?: synchronized(this) {
-                INSTANCE ?: dataBaseBuilder(context).also { INSTANCE = it }
-            }
-        }
 
         fun dataBaseBuilder(context: Context): AppDataBase {
             return Room
                     .databaseBuilder(context, AppDataBase::class.java, "sample-db")
                     .allowMainThreadQueries()
-                    .addCallback(populateDataBase(context))
+                    .addCallback(callBackToPopulateDataBase(context))
                     .build()
 
         }
 
-        private fun populateDataBase(context: Context) = object : RoomDatabase.Callback() {
+        private fun callBackToPopulateDataBase(context: Context) = object : RoomDatabase.Callback() {
             override fun onCreate(db: SupportSQLiteDatabase) {
                 super.onCreate(db)
-                Executors
-                        .newSingleThreadExecutor()
-                        .execute({ getInstance(context).labelDao().insertLabel(PREPOPULATE_DATA) })
+                db.insert(TABLE_LABEL, SQLiteDatabase.CONFLICT_REPLACE, populateDatabase())
             }
         }
 
-        private val PREPOPULATE_DATA = LabelEntity(
-                name = "Nome",
-                email = "Email contato",
-                phone = "Telefone",
-                fantasy_Name = "Nome Empresa",
-                cnpj = "Cnpj",
-                title_mei = "É do tipo MEI ?",
-                positive_option_mei = "Sim",
-                negative_option_mei = "Não")
-
+        private fun populateDatabase(): ContentValues {
+            return ContentValues().apply {
+                put(LABEL_ID, 0)
+                put(LABEL_NAME, "Nome")
+                put(LABEL_EMAIL, "Email contato")
+                put(LABEL_PHONE, "Telefone")
+                put(LABEL_COMPANY_NAME, "Nome Fantasia")
+                put(LABEL_CNPJ, "Cnpj")
+                put(LABEL_TITLE_MEI, "É tipo MEI?")
+                put(LABEL_POSITIVE_OPTION, "Sim")
+                put(LABEL_NEGATIVE_OPTION, "Não")
+            }
+        }
     }
 
 }
